4557c9ff2565bbc874b6a7b175717c9207386b5d
Ray Winkelman <raywinkelman@gmail.com>
2017-07-29 02:24:43 -0300

    Version 1, and the How-To in the README.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Dotployed
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string source;
+            string target;
+
+            if (args.Length == 2)
+            {
+                source = args[0];
+                target = args[1];
+            }
+
+            string[] types = { ".dll", ".exe", ".pdb" };
+
+            IEnumerable<string> files = Directory.GetFiles(target, "*.*", SearchOption.AllDirectories).Where(f => types.Any(t => f.EndsWith(t)));
+
+            DirectoryInfo dir = new DirectoryInfo(source);
+
+            foreach (string file in files)
+            {
+                string assembly = Path.GetFileName(file);
+
+                IEnumerable<FileInfo> fileList = dir.GetFiles("*" + assembly, SearchOption.AllDirectories);
+
+                if (fileList.Any())
+                {
+                    FileInfo targetInfo = new FileInfo(file);
+
+                    fileList = fileList.Where(f => f.CreationTime < targetInfo.CreationTime);
+
+                    if (fileList.Any())
+                    {
+                        string newestFile = fileList.Last().FullName;
+
+                        if (File.Exists(file))
+                        {
+                            File.Delete(file);
+                        }
+
+                        File.Copy(newestFile, file, true);
+
+                        File.SetCreationTime(file, DateTime.Now);
+                        File.SetLastAccessTime(file, DateTime.Now);
+                        File.SetLastWriteTime(file, DateTime.Now);
+
+                        Console.WriteLine(assembly);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Couldn't find: " + assembly + " on the source machine.");
+                }
+            }
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# dot-ployed-dev-tool
-A .NET development tool to deploy assemblies, executables and .pdb files to a testing environment.
+# DotPloyed
+A .NET development tool to deploy assembly, executable and .pdb files to a testing environment.
+
+### Why?
+
+When Visual Studio builds solutions it only builds the projects it detects code changes in. These files get an updated file creation time attribute.
+
+Instead of tracking down the prior versions of said assemblies built in a testing environment to replace them manually, or running an updated installer that includes them, this tool will scan the development environment and detect changes that don't exist in the testing environment to then deploy the updated assemblies and etc.
+
+#### Important Note
+It does a practical file attribute comparison. So If you built projects with changes then installed the software for the first time in the testing environment - the tool won't work until you rebuild.
